9d5e5dc78127709b9c6b4eb931573a1848318268
Joaquín Molina <joaquinmc49b@gmail.com>
2020-04-06 23:48:42 +0200

    Initial commit develop branch

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import configparser
 from base64 import b64encode
 from coinbasepro import CoinBasePro
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+if __name__ == '__main__':
 
-coin_base_pro = CoinBasePro(config['COINBASE_PRO'])
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-coin_base_pro_amount = coin_base_pro.total_amount()
-print(coin_base_pro_amount)
+    coin_base_pro = CoinBasePro(config['COINBASE_PRO'])
+
+    coin_base_pro_amount = coin_base_pro.total_amount()
+    print(coin_base_pro_amount)
+
